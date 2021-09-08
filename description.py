3d47bd221fab4879d535201acd7eb4c2557360e6
alliimarie06 <84410917+alliimarie06@users.noreply.github.com>
2021-09-08 11:31:26 -0400

    description.py
    This explains the differences between the files.

---
+++ description.py
@@ -0,0 +1,30 @@
+"""
+TicTacToeNDFinal.py is a never ending 2-player tic-tac-toe game. 
+The board is set up like
+123
+456
+789
+
+TicTacToeFinal.py is a never ending 2-player tic-tac-toe game.
+The board is set up like
+1|2|3
+-----
+4|5|6
+-----
+7|8|9
+
+TicTacToeSEFinal.py is a 2-player tic-tac-toe game that ends when someone reaches 10 wins.
+The board is set up like 
+1|2|3
+-----
+4|5|6
+-----
+7|8|9
+
+TicTacToeNDESFinal.py is a 2-player tic-tac-toe game that ends when someone reaches 10 wins.
+The board is set up like
+123
+456
+789
+
+"""
